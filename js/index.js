f93f8159783f37ba68b969f81b1bd95edd5e995e
aleupons <apahoquei@gmail.com>
2021-05-29 16:45:42 +0200

    Función busca equipos por provincia

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-const equiposMayoresEdad = (equipos , edad) =>
-equipos.filter((equipo) => equipo.asignado.empleado.edad >= 18);
-console.log(equiposMayoresEdad);
+const equiposMayoresEdad = (equipos, edad) =>
+  equipos.filter((equipo) => equipo.asignado.empleado.edad >= edad);
+
+const equiposProvincia = (equipos, provincia) =>
+  equipos.filter(
+    (equipo) =>
+      equipo.asignado.provincia.toLowerCase() === provincia.toLowerCase()
+  );
